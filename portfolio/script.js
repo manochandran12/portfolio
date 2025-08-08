f9e2ea9597b4090f95c5c99e9ef6a07269f6bf8d
manochandran12 <manomano6868@gmail.com>
2025-08-08 13:45:17 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,7 @@
+/*=====typing animation=====*/
+var typed= new Typed('.typing', {
+    strings: ["Frontend Developer","Web Developer", "Python Developer", "Fullstack Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
